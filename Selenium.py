f1b0a96a5eb2bed682f33a5cde56845026953932
Vorona <arturvorona545@gmail.com>
2026-03-05 21:42:23 +0300

    2

--- Selenium.py
+++ Selenium.py
@@ -27,7 +27,7 @@ last_name_filed.send_keys("Vorona")
 field_value = last_name_filed.get_attribute("value")
 # Проверяем, что оно пустое (равно пустой строке)
 assert field_value == "Vorona"
-time.sleep(1)
+
 
 #Задача 3 ввести значение Email
 import time
@@ -39,7 +39,26 @@ email_field.clear()
 email_field.send_keys("test@example.com")
 field_value_email = email_field.get_attribute("value")
 assert field_value_email == "test@example.com"
+
+#Задача 4 ввести значение MOBILE NUMBER(10)
+import time
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("https://demoqa.com/automation-practice-form")
+number_field = driver.find_element("xpath", "//input[@id='userNumber']")
+number_field.clear()
+number_field.send_keys("9876543210")
+field_value_number = number_field.get_attribute("value")
+assert field_value_number == "9876543210"
+
+
+#Задача 5 ввести значение Current Address
+import time
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("https://demoqa.com/automation-practice-form")
+current_address_field = driver.find_element("xpath", "//textarea[@id='currentAddress']")
+current_address_field.send_keys("123 Main Street, Springfield")
+field_value = current_address_field.get_attribute("value")
+assert field_value == "123 Main Street, Springfield"
 time.sleep(1)
-
-
-
