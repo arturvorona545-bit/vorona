85c11c6e9f13d8a274f566abb55971235ca74ccd
Vorona <arturvorona545@gmail.com>
2026-03-24 19:57:53 +0300

    о

---
+++ Selenium.Dropdown.py
@@ -0,0 +1,6 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+
+driver = webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/dropdown")
