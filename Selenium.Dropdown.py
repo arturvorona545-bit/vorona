bb99bacb6c5d000e97b044458a8e6edd2c48c3b3
Vorona <arturvorona545@gmail.com>
2026-03-25 10:06:40 +0300

    Dropdown

--- Selenium.Dropdown.py
+++ Selenium.Dropdown.py
@@ -2,5 +2,12 @@ import time
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 
+DROPDOWN_ELEMENT = ("xpath", "//select[@id='dropdown']")
+
 driver = webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/dropdown")
+
+
+dropdown = Select(driver.find_element(*DROPDOWN_ELEMENT))
+dropdown.select_by_visible_text("Option 2")
+time.sleep(2)
