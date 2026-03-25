0fbb6d3f48ebe894132d09ecf5411cef8ceb1967
Vorona <arturvorona545@gmail.com>
2026-03-25 11:03:21 +0300

    MULTI_SELECT

---
+++ Selenium.multi Select.py
@@ -0,0 +1,32 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
+
+MULTISELECT = ("xpath","//input[@id='react-select-4-input']")
+
+driver = webdriver.Chrome()
+driver.get("https://demoqa.com/select-menu")
+
+select = driver.find_element(*MULTISELECT)
+select.send_keys("Green")
+assert select.get_attribute("value") == "Green", "Error in value Green"
+select.send_keys(Keys.ENTER)
+
+select = driver.find_element(*MULTISELECT)
+select.send_keys("Blue")
+assert select.get_attribute("value") == "Blue", "Error in value Blue"
+select.send_keys(Keys.ENTER)
+
+select = driver.find_element(*MULTISELECT)
+select.send_keys("Black")
+assert select.get_attribute("value") == "Black", "Error in value Black"
+select.send_keys(Keys.ENTER)
+
+select = driver.find_element(*MULTISELECT)
+select.send_keys("Red")
+assert select.get_attribute("value") == "Red", "Error in value Red"
+select.send_keys(Keys.ENTER)
+select.send_keys(Keys.ESCAPE)
+
+time.sleep(5)
