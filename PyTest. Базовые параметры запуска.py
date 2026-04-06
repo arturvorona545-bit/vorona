75d3d43580de2a1a5b17b24141facdb2f1c22165
Vorona <arturvorona545@gmail.com>
2026-04-06 19:52:34 +0300

    Базовые параметры запуска

--- PyTest. Базовые параметры запуска.py
+++ PyTest. Базовые параметры запуска.py
@@ -1,1 +1,15 @@
-assert 5 == 5, "Error"
+from selenium import webdriver
+
+class TestExample:
+
+    def test_login(self):
+        driver = webdriver.Chrome()
+        driver.get("https://demoqa.com/login")  # исправлено: https и demoqa
+        assert driver.current_url == "https://demoqa.com/login", "открыта некорректная страница"
+        driver.quit()
+
+    def test_login2(self):
+        driver = webdriver.Chrome()
+        driver.get("https://demoqa.com/login")  # исправлено: https и demoqa
+        assert driver.current_url == "https://demoqa.com/login", "открыта некорректная страница"
+        driver.quit()
